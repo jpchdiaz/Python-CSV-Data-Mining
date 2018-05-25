5eef68124e1c00706b9dfe1c5e86a1e86c459f93
jpitched <jpchdiaz@gmail.com>
2018-05-25 11:36:06 -0400

    PyBoss solvedgit add -A!

--- PyBoss/pybosssolution.py
+++ PyBoss/pybosssolution.py
@@ -1,10 +1,62 @@
 import csv
-import datetime
 
 #define where csv is
 datapath = "raw_data/employee_data1.csv"
 
 new_employee_list = []
+
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
 
 with open(datapath) as csvfile:
     reader = csv.DictReader(csvfile)
@@ -20,21 +72,22 @@ with open(datapath) as csvfile:
         #SSN
         lastssn = row["SSN"].split('-')[2]
         newssn = f"***-**-{lastssn}"
-        #State
+        #State -- thanks, dictionaries.py from session 3!
         state = row["State"]
+        abbrev = f"{us_state_abbrev[state]}"
         new_employee_list.append(
             {   "Emp ID": row["Emp ID"],
                 "First Name": row["Name"].split()[0],
                 "Last Name": row["Name"].split()[1],
                 "DOB": newdate,
                 "SSN": newssn,
-                "State": "State"
+                "State": abbrev
             }
         )
 
-newpath = 'newemployees.csv'
-with open(newpath, "w") as csvfile:
+newpath = 'newemployees_1.csv'
+with open(newpath, "w", newline='') as csvfile:
     fieldnames = ["Emp ID", "First Name", "Last Name", "DOB", "SSN", "State"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(new_employee_data)
+    writer.writerows(new_employee_list)
--- PyBoss/pybosssolution2.py
+++ PyBoss/pybosssolution2.py
@@ -5,9 +5,60 @@ datapath = "raw_data/employee_data1.csv"
 
 new_employee_list = []
 
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
 with open(datapath) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        lastssn = row["SSN"].split('-')[2]
-        newssn = f"***-**-{lastssn}"
-        print(newssn)
+        print(f'{us_state_abbrev[row["State"]]}')
